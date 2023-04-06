5b87fefc2c2d8e01fcf55a9cefedb05f51c1c709
joseCarlosAndrade <josecarlos.101@usp.br>
2023-04-05 23:08:27 -0300

    fixed comment

--- c/codeforces/1807F-bouncy.c
+++ c/codeforces/1807F-bouncy.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 
-# problem: https://codeforces.com/problemset/problem/1807/F
+// problem: https://codeforces.com/problemset/problem/1807/F
 
 typedef struct Bouncy
 {
